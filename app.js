4d0738d8e9b5dc0bc2a8334b6a036bc9d9dddd61
samuel21695 <samuel21695@gmail.com>
2023-10-12 11:36:24 +0900

    conflict test2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead (200, {"Content-Type": "text/plain" });
-  res.write('향마루 똥띵땅똥또옹똥똥띵땅띵똥 삥삥삥빵삥뽀옹빵삥삥삥빵삥뽕');
+  res.write('뛰뛰빵빵 뛰뛰빵빵');
   res.end();
 });
 
