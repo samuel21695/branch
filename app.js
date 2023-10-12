f239d154684a49cd38132ffcd4728212e1d35d72
samuel21695 <samuel21695@gmail.com>
2023-10-12 10:57:34 +0900

    향마루 응답 처리

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-
+  res.writeHead (200, {"Content-Type": "text/plain" });
+  res.write('향마루');
+  res.end();
 });
 
-server.listen(3500, (err) => {
-  if (!err) {
-    console.error ('server is not working')
-  } else {
-    console.error ('successfully working on port 3500 http://localhost:3500')
-  }
-})
+server.listen(3500);
